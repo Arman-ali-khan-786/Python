ea9a7f932cd51ef9d89048c8b00d7f74809d86a4
ARMAN ALI KHAN <72055054+Arman-ali-khan-786@users.noreply.github.com>
2021-05-21 12:11:32 +0530

    uploaded python files

---
+++ patterns/to print pattern#39.py
@@ -0,0 +1,6 @@
+for a in range(5,0,-1):
+    for b in range(1,a):
+        print(" ",end="")
+    for c in range(5,a-1,-1):
+        print(a,end="")
+    print()
---
+++ patterns/to print pattern#40.py
@@ -0,0 +1,6 @@
+for a in range(1,6):
+    for b in range(a,5):
+        print(" ",end="")
+    for c in range(1,a+1):
+        print(c%2,end="")
+    print()
