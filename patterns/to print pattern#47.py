ed650705b2f6aeb99af487bbe49e641319272165
ARMAN ALI KHAN <72055054+Arman-ali-khan-786@users.noreply.github.com>
2021-05-22 16:03:43 +0530

    pattern #47 & #48

---
+++ patterns/to print pattern#47.py
@@ -0,0 +1,6 @@
+for a in range(1,6):
+    for b in range(1,a):
+        print(" ",end="")
+    for c in range(a,6):
+        print(a,end="")
+    print()
---
+++ patterns/to print pattern#48.py
@@ -0,0 +1,6 @@
+for a in range(0,5):
+    for b in range(1,a+1):
+        print(" ",end="")
+    for c in range(5-a,0,-1):
+        print(c,end="")
+    print()
